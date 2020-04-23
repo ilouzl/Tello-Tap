76c50d56b1280f6d2aac3e9a7982435f969d234e
Liron Ilouz <liron@tapwithus.com>
2020-04-23 20:59:06 +0300

    New tello class with stats

--- drone.py
+++ drone.py
@@ -2,52 +2,40 @@ import curses
 import socket
 from time import sleep
 import asyncio
-from easytello import tello
-
-my_drone = tello.Tello()
-
-INTERVAL = 0.2
+from easytello.tello import Tello
 
 
-stats = {}
+class MyTello(Tello):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.stats = {}
+        self.local_port_stats = 8890
+        self.stats_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket for sending cmd
+        self.stats_socket.bind((self.local_ip, self.local_port_stats))
+        self.stats_socket.sendto('command'.encode('utf-8'), self.tello_address)
+    
+    def read_stats(self):
+        response, ip = self.stats_socket.recvfrom(1024)
+        if response == 'ok':
+            return
+        response = response.decode('utf8')
+        out = response.replace(';', ';\n')
+        if self.debug is True:
+            print('Tello State:\n' + out)
+        # for s in response.split(';'):
+        #     v = s.split(':')
+        #     self.stats[v[0]] = int(v[1])
 
-def report(str):
-    stdscr.addstr(0, 0, str)
-    stdscr.refresh()
+    def loop_stats(self):
+        while True:
+            self.read_stats()
+            sleep(0.2)
+        
 
 if __name__ == "__main__":
     stdscr = curses.initscr()
     curses.noecho()
     curses.cbreak()
 
-    local_ip = ''
-    local_port = 8890
-    socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket for sending cmd
-    socket.bind((local_ip, local_port))
-
-    tello_ip = '192.168.10.1'
-    tello_port = 8889
-    tello_adderss = (tello_ip, tello_port)
-
-    socket.sendto('command'.encode('utf-8'), tello_adderss)
-
-    try:
-        index = 0
-        while True:
-            index += 1
-            response, ip = socket.recvfrom(1024)
-            if response == 'ok':
-                continue
-            response = response.decode('utf8')
-            out = response.replace(';', ';\n')
-            out = 'Tello State:\n' + out
-            for s in response.split(';'):
-                v = s.split(':')
-                stats[v[0]] = int(v[1])
-
-            report(str(stats))
-            sleep(INTERVAL)
-    except KeyboardInterrupt:
-        curses.echo()
-        curses.nocbreak()
-        curses.endwin()
+    my_drone = MyTello(tello_ip="127.0.0.1")
+    my_drone.loop_stats()
